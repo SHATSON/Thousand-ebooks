d301da50a43aad590403edf6381602e4008be084
Ravi Kishore <ravikishore1993@gmail.com>
2013-02-06 00:44:00 +0530

    Almost done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+$(document).ready(function() {
+    $('#maintable').dataTable({
+
+    	"bPaginate": false,
+        "bLengthChange": false,
+
+        "bSort": true,
+        "bInfo": false,
+        "bAutoWidth": false
+
+    });
+
+    $($('#maintable_filter').children()[0]).attr('class' , 'help-inline' )
+} );
 
